cfd3497d986b9df84dc557513ec720d595d884dd
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-03-17 17:57:42 +0530

    commands class

--- picoturtle-dotnet/picoturtle/Turtle.cs
+++ picoturtle-dotnet/picoturtle/Turtle.cs
@@ -1,10 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
 namespace picoturtle
 {
     public class Turtle
     {
-        public Turtle()
+        private string name;
+        private string turtleUrl;
+        private bool bulk;
+        private int bulkLimit;
+        private List<TurtleCommand> commands;
+
+        public Turtle(string name = null,
+                 string host = "127.0.0.1",
+                 int port = 3000,
+                 bool bulk = true,
+                 int bulkLimit = 100)
         {
+            this.name = name;
+            this.turtleUrl = "http://" + host + ":" + port;
+            this.bulk = bulk;
+            this.bulkLimit = bulkLimit;
+            this.name = name;
+            this.commands = new List<TurtleCommand>();
+            if (this.name == null) {
+                //self.turtle_init()
+
+            }
+            this.createRequest(null);
+        }
+
+        private void createRequest(String cmd)
+        {
+            // Create a New HttpClient object and dispose it when done, so the app doesn't leak resources
+            using (HttpClient client = new HttpClient())
+            {
+                // Call asynchronous network methods in a try/catch block to handle exceptions
+                try
+                {
+                    Task<string> resTask = client.GetStringAsync("http://www.abhishekmishra.in/");
+                    resTask.Wait();
+                    string response = resTask.Result;
+
+                    Debug.WriteLine(response);
+                }
+                catch (HttpRequestException e)
+                {
+                    Debug.WriteLine("\nException Caught!");
+                    Debug.WriteLine("Message :{0} ", e.Message);
+                }
+            }
         }
     }
 }
---
+++ picoturtle-dotnet/picoturtle/TurtleCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace picoturtle
+{
+    public class TurtleCommand
+    {
+        private string commandName { get; }
+        private List<string> valueList { get; set; }
+        private Dictionary<string, string> valueObj { get; set; }
+        private bool valueIsObj { get; }
+
+        public TurtleCommand(string cmd, bool isObj)
+        {
+            commandName = cmd;
+            valueIsObj = isObj;
+            if(valueIsObj)
+            {
+                valueObj = new Dictionary<string, string>();
+            }
+            else
+            {
+                valueList = new List<string>();
+            }
+        }
+    }
+}
