bc6e87698a80b0036b910ded1e0f4c52167d4bfe
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-03-17 13:05:12 +0530

    added initial project/soln

---
+++ picoturtle-dotnet/picoturtle-dotnet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using picoturtle;
+
+namespace picoturtledotnet
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Turtle t = new picoturtle.Turtle();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
