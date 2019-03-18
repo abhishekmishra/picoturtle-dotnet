a3b1f62f011200b3ec1fd1293bb9bbfaded6598f
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-03-18 13:43:14 +0530

    added all turtle commands

--- picoturtle-dotnet/picoturtle-dotnet/Program.cs
+++ picoturtle-dotnet/picoturtle-dotnet/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using picoturtle;
 
 namespace picoturtledotnet
@@ -8,7 +9,17 @@ namespace picoturtledotnet
         public static void Main(string[] args)
         {
             Turtle t = new picoturtle.Turtle();
+            t.Init(250, 250);
             Console.WriteLine("Created Turtle with name -> " + t.name);
+            t.pendown();
+            t.pencolour(128, 128, 0);
+            for (int i = 0; i < 4; i++)
+            {
+                t.forward(100);
+                t.right(90);
+            }
+            t.stop();
+            Process.Start(t.BrowserURL());
         }
     }
 }
--- picoturtle-dotnet/picoturtle/Turtle.cs
+++ picoturtle-dotnet/picoturtle/Turtle.cs
@@ -9,6 +9,7 @@ namespace picoturtle
 {
     public class Turtle
     {
+
         public string name { get; set; }
         private string turtleUrl;
         private bool bulk;
@@ -29,18 +30,18 @@ namespace picoturtle
             this.name = name;
             this.commands = new List<TurtleCommand>();
             client = new HttpClient();
-            if (this.name == null)
-            {
-                this.Init();
-            }
+            //if (this.name == null)
+            //{
+            //    this.Init();
+            //}
         }
 
-        private TurtleState CreateRequest(String cmd, List<KeyValuePair<string, Object>> args, bool is_obj = false)
+        private TurtleState TurtleRequest(String cmd, List<KeyValuePair<string, Object>> args = null, bool is_obj = false)
         {
             if (this.bulk == true && (cmd != "create"))
             {
                 TurtleCommand command = new TurtleCommand(cmd);
-                if (args.Count > 0)
+                if (args != null && args.Count > 0)
                 {
                     if (is_obj)
                     {
@@ -62,19 +63,23 @@ namespace picoturtle
                 this.commands.Add(command);
                 if ((this.commands.Count >= this.bulkLimit) || (cmd == "stop") || (cmd == "state"))
                 {
-                    //print(this.commands)
-                    //drain the commands
+                    //Debug.WriteLine("Draining commands " + this.commands.Count);
                     string json = JsonConvert.SerializeObject(this.commands);
+                    //Debug.WriteLine(json);
+                    string url = this.turtleUrl + "/turtle/" +
+                                             this.name + "/commands";
+                    //Debug.WriteLine(url);
                     try
                     {
-                        Task<HttpResponseMessage> resTask = client.PostAsync(this.turtleUrl + "/turtle/" +
-                                             this.name + "/commands", new StringContent(json));
+                        Task<HttpResponseMessage> resTask = client.PostAsync(url, new StringContent(json, System.Text.Encoding.UTF8, "application/json"));
                         resTask.Wait();
                         Task<string> responseTextTask = resTask.Result.Content.ReadAsStringAsync();
                         responseTextTask.Wait();
                         string responseText = responseTextTask.Result;
 
-                        Debug.WriteLine(responseText);
+                        //Debug.WriteLine(responseText);
+                        var t = JsonConvert.DeserializeObject<TurtleState>(responseText);
+                        return t;
                     }
                     catch (HttpRequestException e)
                     {
@@ -82,51 +87,44 @@ namespace picoturtle
                         Debug.WriteLine("Message :{0} ", e.Message);
                     }
 
-                    //    req = urllib.request.Request(this.turtle_url + "/turtle/" +
-                    //                             this.name + "/commands")
-                    //req.add_header("Content-Type",
-                    //               "application/json; charset=utf-8")
-                    //jsondata = json.dumps(this.commands)
-                    //jsondataasbytes = jsondata.encode("utf-8")  # needs to be bytes
-                    //req.add_header("Content-Length", len(jsondataasbytes))
-                    //res = urllib.request.urlopen(req, jsondataasbytes)
-                    //# print("Draining " + str(len(this.commands)) + " commands for " +
-                    //#       this.name)
-                    //this.commands = []
-                    //t = json.loads(res.read().decode("utf-8"))
-                    //return t
+                    this.commands = new List<TurtleCommand>();
                 }
             }
             else
             {
                 var request_url = "/turtle/";
-                if (this.name != null) {
+                if (this.name != null)
+                {
                     request_url += this.name;
                     request_url += "/";
                 }
                 request_url += cmd;
-                request_url += "?";
-                int i = 0;
-                foreach (var arg in args)
+                if (args != null)
                 {
-                    if (i > 0)
+                    request_url += "?";
+                    int i = 0;
+                    foreach (var arg in args)
                     {
-                        request_url += "&";
+                        if (i > 0)
+                        {
+                            request_url += "&";
+                        }
+                        request_url += arg.Key;
+                        request_url += "=";
+                        request_url += arg.Value.ToString();
+                        i++;
                     }
-                    request_url += arg.Key;
-                    request_url += "=";
-                    request_url += arg.Value.ToString();
                 }
                 string json = JsonConvert.SerializeObject(this.commands);
                 try
                 {
+                    //Debug.WriteLine(request_url);
                     Task<string> resTask = client.GetStringAsync(this.turtleUrl + request_url);
                     resTask.Wait();
                     string responseText = resTask.Result;
 
-                    Debug.WriteLine(responseText);
+                    //Debug.WriteLine(responseText);
                     var t = JsonConvert.DeserializeObject<TurtleState>(responseText);
-                    Debug.WriteLine(t.location.x);
                     return t;
                 }
                 catch (HttpRequestException e)
@@ -138,15 +136,159 @@ namespace picoturtle
             return null;
         }
 
-        TurtleState Init()
+        public string BrowserURL()
+        {
+            return turtleUrl + "/index.html?details=0&list=0&name=" + name;
+        }
+
+        public TurtleState Init(double x=250, double y=250)
         {
             commands = new List<TurtleCommand>();
             var args = new List<KeyValuePair<string, Object>>();
-            args.Add(new KeyValuePair<string, Object>("x", 250));
-            args.Add(new KeyValuePair<string, Object>("y", 250));
-            TurtleState t = CreateRequest("create", args);
+            args.Add(new KeyValuePair<string, Object>("x", x));
+            args.Add(new KeyValuePair<string, Object>("y", y));
+            TurtleState t = TurtleRequest("create", args);
             this.name = t.name;
             return t;
         }
+
+        public TurtleState penup()
+        {
+            return TurtleRequest("penup");
+        }
+
+        public TurtleState pendown()
+        {
+            return TurtleRequest("pendown");
+        }
+
+        public TurtleState penwidth(double w)
+        {
+            var args = new List<KeyValuePair<string, Object>>();
+            args.Add(new KeyValuePair<string, Object>("w", w));
+
+            return TurtleRequest("penwidth", args);
+        }
+
+        public TurtleState stop()
+        {
+            return TurtleRequest("stop");
+        }
+
+        public TurtleState state()
+        {
+            return TurtleRequest("state");
+        }
+
+        public TurtleState home()
+        {
+            return TurtleRequest("home");
+        }
+
+        public TurtleState clear()
+        {
+            return TurtleRequest("clear");
+        }
+
+        public TurtleState forward(double d)
+        {
+            var args = new List<KeyValuePair<string, Object>>();
+            args.Add(new KeyValuePair<string, Object>("d", d));
+
+            return TurtleRequest("forward", args);
+        }
+
+        public TurtleState back(double d)
+        {
+            var args = new List<KeyValuePair<string, Object>>();
+            args.Add(new KeyValuePair<string, Object>("d", d));
+
+            return TurtleRequest("back", args);
+        }
+
+        public TurtleState setpos(double x, double y)
+        {
+            var args = new List<KeyValuePair<string, Object>>();
+            args.Add(new KeyValuePair<string, Object>("x", x));
+            args.Add(new KeyValuePair<string, Object>("y", y));
+
+            return TurtleRequest("goto", args);
+        }
+
+        public TurtleState setx(double x)
+        {
+            var args = new List<KeyValuePair<string, Object>>();
+            args.Add(new KeyValuePair<string, Object>("x", x));
+
+            return TurtleRequest("setx", args);
+        }
+
+        public TurtleState sety(double y)
+        {
+            var args = new List<KeyValuePair<string, Object>>();
+            args.Add(new KeyValuePair<string, Object>("y", y));
+
+            return TurtleRequest("sety", args);
+        }
+
+        public TurtleState left(double a)
+        {
+            var args = new List<KeyValuePair<string, Object>>();
+            args.Add(new KeyValuePair<string, Object>("a", a));
+
+            return TurtleRequest("left", args);
+        }
+
+        public TurtleState right(double a)
+        {
+            var args = new List<KeyValuePair<string, Object>>();
+            args.Add(new KeyValuePair<string, Object>("a", a));
+
+            return TurtleRequest("right", args);
+        }
+
+        public TurtleState heading(double a)
+        {
+            var args = new List<KeyValuePair<string, Object>>();
+            args.Add(new KeyValuePair<string, Object>("a", a));
+
+            return TurtleRequest("heading", args);
+        }
+
+        public TurtleState font(string f)
+        {
+            var args = new List<KeyValuePair<string, Object>>();
+            args.Add(new KeyValuePair<string, Object>("f", f));
+
+            return TurtleRequest("font", args);
+        }
+
+        public TurtleState filltext(string text)
+        {
+            var args = new List<KeyValuePair<string, Object>>();
+            args.Add(new KeyValuePair<string, Object>("t", text));
+
+            return TurtleRequest("filltext", args);
+        }
+
+        public TurtleState stroketext(string text)
+        {
+            var args = new List<KeyValuePair<string, Object>>();
+            args.Add(new KeyValuePair<string, Object>("t", text));
+
+            return TurtleRequest("stroketext", args);
+        }
+
+        public TurtleState pencolour(int r, int g, int b)
+        {
+            var args = new List<KeyValuePair<string, Object>>();
+            args.Add(new KeyValuePair<string, Object>("r", r));
+            args.Add(new KeyValuePair<string, Object>("g", g));
+            args.Add(new KeyValuePair<string, Object>("b", b));
+
+            return TurtleRequest("pencolour", args, true);
+        }
     }
+
+
 }
